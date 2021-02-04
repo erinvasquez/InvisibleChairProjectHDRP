20476f587dcd35c836191caa992fe6f5fc069044
Erin Vasquez <erin.vasquez.438@my.csun.edu>
2021-02-04 15:09:16 -0800

    Create MovingSphere.cs

---
+++ Assets/Scripts/MovingSphere.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 
+/// </summary>
+public class MovingSphere : MonoBehaviour {
+
+    [SerializeField]
+
+
+    // Start is called before the first frame update
+    void Awake() {
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private void Update() {
+        
+    }
+
+}
