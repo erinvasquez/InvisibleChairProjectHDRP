c985341966e0a616f7ec75b164737560a4ebae2f
Erin Vasquez <erin.vasquez@gmail.com>
2021-06-21 01:09:37 -0700

    Added Mouse Look script, quick pause on ESC
    Only a basic script but a reliable one now with the new input system. We'll replace first person cube with an actual player object eventually.

---
+++ Assets/Scripts/Camera/CameraLook.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraLook : MonoBehaviour {
+    public Camera cam;
+
+    Transform player;
+
+    public float sensitivityX = .033f;
+    public float sensitivityY = .033f;
+    public float minY = -89f;
+    public float maxY = 89f;
+
+    public float lookX;
+    public float lookY;
+
+    void Start() {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        player = transform.parent;
+        cam = Camera.main;
+
+    }
+
+    void Update() {
+
+        // Add some way to stop our camera from "looking" while paused
+        // Hook this in with any pause menu manager functionalities
+        if (Keyboard.current.escapeKey.wasPressedThisFrame) {
+            ToggleCursorMode();
+        }
+
+        if (Cursor.lockState == CursorLockMode.Locked) {
+            Look();
+        }
+
+    }
+
+    /// <summary>
+    /// Handle our mouse look input from below
+    /// </summary>
+    public void Look() {
+
+        lookY = Mathf.Clamp(lookY, minY, maxY);
+
+        player.localEulerAngles = new Vector3(0, lookX, 0);
+
+        cam.transform.localEulerAngles = new Vector3(lookY, 0, 0);
+
+    }
+
+    
+    /// <summary>
+    /// Get Mouse look X delta
+    /// </summary>
+    /// <param name="context"></param>
+    public void OnLookX(InputAction.CallbackContext context) {
+        lookX += context.ReadValue<float>() * sensitivityX /** Time.deltaTime*/;
+    }
+
+    /// <summary>
+    /// Get Mouse look Y delta
+    /// </summary>
+    /// <param name="context"></param>
+    public void OnLookY(InputAction.CallbackContext context) {
+        lookY -= context.ReadValue<float>() * sensitivityY /** Time.deltaTime*/;
+    }
+
+    private void ToggleCursorMode() {
+        Cursor.visible = !Cursor.visible;
+
+        if (Cursor.lockState == CursorLockMode.None) {
+            Cursor.lockState = CursorLockMode.Locked;
+        } else {
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+    }
+
+}
